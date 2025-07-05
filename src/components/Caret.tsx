47a2c26699bdc834ba711b6bb4d6dad6b77f245f
mxte-b <182339124+mxte-b@users.noreply.github.com>
2025-07-05 11:54:53 +0200

    Caret movement, input tracking

--- src/components/Caret.tsx
+++ src/components/Caret.tsx
@@ -1,12 +1,17 @@
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import type { CaretPosition } from "../types/general";
 
+const LETTER_WIDTH = 15;
+
 const Caret = ({ position }: { position: CaretPosition }) => {
+    const caretRef = useRef<HTMLDivElement>(null);
+
     useEffect(() => {
-        console.log("new position: " + position.letterId)
+        if (!caretRef.current) return;
+        caretRef.current.style.transform = `translate3d(${position.letterId * (LETTER_WIDTH + 1)}px, 0px, 0px)`
     }, [position])
 
-    return <div className="caret"/>
+    return <div className="caret" ref={caretRef}/>
 }
 
 export default Caret;
--- src/components/TypingArea.tsx
+++ src/components/TypingArea.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react"
+import { useEffect, useRef, useState, type ClipboardEventHandler, type FocusEventHandler, type FormEventHandler, type KeyboardEventHandler } from "react"
 import splitText from "../helpers/SplitText"
 import type { CaretPosition } from "../types/general";
 import Caret from "./Caret";
@@ -9,34 +9,89 @@ const TypingArea = ({ quote }: { quote: string }) => {
 
     const inputRef = useRef<HTMLInputElement>(null);
     const isInitialized = useRef<boolean>(false);
+    const lastInputValue = useRef<string>("");
 
+    // Event listeners
+    const inputHandler: FormEventHandler<HTMLInputElement> = (e) => {
+        const text = (e.target as HTMLInputElement).value;
+        const delta = text.length - lastInputValue.current.length;
+        
+        // If delta is bigger than one, or it is zero, than it is pasted
+        if (delta > 1 || delta == 0) {
+            (e.target as HTMLInputElement).value = lastInputValue.current;
+            return;
+        }
+        // Check new letter correctness
+        else if (delta > 0) {
+            console.log("Checking")
+            const key = text[text.length - 1]
+            const quoteChar = quote[currentPosition.letterId]
+
+            // console.log(key)
+        }
+        // Remove active classes from inactive characters
+        else {
+            console.log("remove")
+        }
+        
+        console.log(text, delta)
+        
+        // Toggle character state based on correctness
+
+        // Update caret position
+        setCurrentPosition(prev => {
+            const newPosition: CaretPosition = { wordId: prev.wordId, letterId: prev.letterId + delta };
+            return newPosition
+        })
+        
+        lastInputValue.current = text
+    }
+
+    const focusHandler: FocusEventHandler<HTMLInputElement> = (e) => {
+        if (!inputRef.current) return;
+
+        inputRef.current.classList.add("focused")
+    }
+
+    const blurHandler: FocusEventHandler<HTMLInputElement> = (e) => {
+        if (!inputRef.current) return;
+
+        // inputRef.current.classList.remove("focused")
+        inputRef.current.focus()
+    }
+
+    // Preventing pasting into the text input
+    const pasteHandler: ClipboardEventHandler<HTMLInputElement>  = (e) => {
+        e.preventDefault();
+    }
+
+    // Auto-focus on the typing area
     useEffect(() => {
         if (!inputRef.current || isInitialized.current) return;
-
         isInitialized.current = true;
 
         // Attach focus to the text input
         inputRef.current.focus();
-
-        // Attach event listener to the text input
-        const keyPressListener: (e: KeyboardEvent) => any = (e) => {
-            setCurrentPosition(prev => {
-                const newPosition: CaretPosition = { wordId: prev.wordId, letterId: prev.letterId + 1 };
-                return newPosition
-            })
-        }
-
-        inputRef.current.addEventListener("keypress", keyPressListener)
-
-        // Return cleanup function
-        return () => {
-            inputRef.current?.removeEventListener("keypress", keyPressListener)
-        }
     }, [])
 
     return (
         <div className="typing-test">
-            <input type="text" ref={inputRef} className="text-input" id="textInput" autoComplete="off" autoCapitalize="off" autoCorrect="off" data-gramm="false" data-enable-grammarly="false" spellCheck="false" />
+            <input 
+                type="text" 
+                ref={inputRef} 
+                onChange={inputHandler} 
+                onFocus={focusHandler}
+                onBlur={blurHandler}
+                onPaste={pasteHandler}
+                className="text-input" 
+                id="textInput" 
+                autoComplete="off" 
+                autoCapitalize="off" 
+                autoCorrect="off" 
+                data-gramm="false" 
+                data-enable-grammarly="false" 
+                spellCheck="false" 
+            />
             <div className="text-container color-secondary">
                 { splitText(quote) }
             </div>
